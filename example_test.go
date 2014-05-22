90f1f3308af492b11afc9c4f8b554dda30b382c4
Billy Rond <billy.rond@gmail.com>
2014-05-21 20:11:23 -0600

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -9,7 +9,7 @@ func ExampleHelloworld() {
 }
 
 // from http://stackoverflow.com/questions/19906544/
-func ExampleArange((start, stop, step float64) []float64 {
+func ExampleArange(start, stop, step float64) []float64 {
 	N := int(math.Ceil((stop - start) / step))
 	rnge := make([]float64, N)
 	for x := range rnge {
