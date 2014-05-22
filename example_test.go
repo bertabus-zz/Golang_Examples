2b08d1314ec7b26b9a8a72315c33f470e081eb5c
Billy Rond <billy.rond@gmail.com>
2014-05-21 20:17:39 -0600

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -8,12 +8,11 @@ func ExampleHelloworld() {
 	fmt.Println("hello world")
 }
 
-// from http://stackoverflow.com/questions/19906544/
-func ExampleArange(start, stop, step float64) []float64 {
-	N := int(math.Ceil((stop - start) / step))
-	rnge := make([]float64, N)
-	for x := range rnge {
-		rnge[x] = start + step*float64(x)
-	}
-	return rnge
+func Arange(start, stop, step float64) []float64 {
+    N := int(math.Ceil((stop - start) / step))
+    rnge := make([]float64, N)
+    for x := range rnge {
+        rnge[x] = start + step*float64(x)
+    }
+    return rnge
 }
