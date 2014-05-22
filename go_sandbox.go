fb523d88788cfee08095b6557a5ea058032bba8a
Billy Rond <billy.rond@gmail.com>
2014-05-21 20:28:25 -0600

    Update go_sandbox.go

--- go_sandbox.go
+++ go_sandbox.go
@@ -4,7 +4,13 @@ package go_sandbox
 func Helloworld() {
 }
 
-//like python/numpy arange function
+//python-numpy like arange function
+//from http://stackoverflow.com/questions/19906544/
 func Arange(start, stop, step float64) []float64 {
-  return 1.1
+    N := int(math.Ceil((stop - start) / step))
+    rnge := make([]float64, N)
+    for x := range rnge {
+        rnge[x] = start + step*float64(x)
+    }
+    return rnge
 }
